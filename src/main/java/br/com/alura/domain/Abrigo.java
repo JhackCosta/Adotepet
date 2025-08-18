906078aac0e82bcad529000a47449209ba34fe3f
Jhack Rychard De Oliveira Costa <jhack.costa@sankhya.com.br>
2025-08-17 23:40:40 -0300

    refactor: aplica lib jackson-databind

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -1,17 +1,38 @@
 package br.com.alura.domain;
 
-import com.google.gson.Gson;
 
 public class Abrigo {
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
+    private Pet[] pets;
 
+    public Abrigo(){}
     public Abrigo(String telefone, String nome, String email) {
         this.telefone = telefone;
         this.nome = nome;
         this.email = email;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
